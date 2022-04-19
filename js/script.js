4727a978d7fd6eb0410ff3bf78e6b68783334fb5
tbable <legen_nick@mail.ru>
2022-04-19 06:09:11 +0400

    fourth commit

--- js/script.js
+++ js/script.js
@@ -71,10 +71,3 @@ writeYourGenres();
 
 showMyDB();
 
-function writeYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, "");
-	}
-}
-
-writeYourGenres();
